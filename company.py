cc9140e86646133349832e98e9d80cf287082832
takulenko <t.akulenko@quality-lab.ru>
2025-02-05 13:02:28 +0300

    Созданы классы contact и company (без их использования)

---
+++ company.py
@@ -0,0 +1,6 @@
+
+class Company:
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+
---
+++ contact.py
@@ -0,0 +1,24 @@
+
+class Contact:
+    def __init__(self, firstname, middlename, lastname, nickname, phonehome, email, email2, email3, mobile, phonework, fax, photo, title, homepage, bday, bmonth, byear, aday, amonth, ayear):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.phonehome = phonehome
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.mobile = mobile
+        self.phonework = phonework
+        self.fax = fax
+        self.photo = photo
+        self.title = title
+        self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+
