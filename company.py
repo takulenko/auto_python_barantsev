4ca3b694a1df708103240c61b3331af43bd8f8cd
takulenko <t.akulenko@quality-lab.ru>
2025-02-05 13:12:09 +0300

    Отредактирован класс contact (без его использования)

--- company.py
+++
@@ -1,6 +0,0 @@
-
-class Company:
-    def __init__(self, name, address):
-        self.name = name
-        self.address = address
-
--- contact.py
+++ contact.py
@@ -1,20 +1,22 @@
 
 class Contact:
-    def __init__(self, firstname, middlename, lastname, nickname, phonehome, email, email2, email3, mobile, phonework, fax, photo, title, homepage, bday, bmonth, byear, aday, amonth, ayear):
+    def __init__(self, firstname, middlename, lastname, nickname, photo, title, company, address, phonehome, mobile, phonework, fax, email, email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
         self.nickname = nickname
-        self.phonehome = phonehome
-        self.email = email
-        self.email2 = email2
-        self.email3 = email3
-        self.mobile = mobile
-        self.phonework = phonework
-        self.fax = fax
         self.photo = photo
         self.title = title
         self.homepage = homepage
+        self.company = company
+        self.address = address
+        self.phonehome = phonehome
+        self.mobile = mobile
+        self.phonework = phonework
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
         self.bday = bday
         self.bmonth = bmonth
         self.byear = byear
