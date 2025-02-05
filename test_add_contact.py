0696843ab7b551c7c5c42bd917bf8ee0de1303de
takulenko <t.akulenko@quality-lab.ru>
2025-02-05 12:12:45 +0300

    Дополнено заполнение всех полей. Не решена проблема загрузки photo.
    Выполнен рефакторинг параметров.
    
     test_add_contact

--- test_add_contact.py
+++ test_add_contact.py
@@ -21,31 +21,82 @@ class TestAddContact(unittest.TestCase):
     def go_home_page(self, wd):
         wd.find_element_by_link_text("home").click()
 
-    def add_contact(self, wd):
+    def add_contact(self, wd, contact_firstname="First_name", contact_middlename="Middle_name", contact_lastname="Last_name",
+                    contact_nickname="Nick", contact_phonehome="123", contact_email="contact@mail.ru", contact_bday="1",
+                    contact_bmonth="February", contact_byear="2002", contact_photo="photo.jpg", contact_title="11",
+                    company="11", company_address="11", contact_mobile="11", contact_phonework="11", contact_fax="11",
+                    contact_email2="11", contact_email3="11", contact_homepage="11", contact_aday="26",
+                    contact_amonth="November", contact_ayear="2010"):
         # init contact creation
         wd.find_element_by_link_text("add new").click()
+
         # fill contact form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("First_name")
+        wd.find_element_by_name("firstname").send_keys(contact_firstname)
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("Middle_name")
+        wd.find_element_by_name("middlename").send_keys(contact_middlename)
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Last_name")
+        wd.find_element_by_name("lastname").send_keys(contact_lastname)
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("Nick")
+        wd.find_element_by_name("nickname").send_keys(contact_nickname)
+
+        wd.find_element_by_name("photo").clear()
+#        wd.find_element_by_name("photo").send_keys(contact_photo)
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(contact_title)
+
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(company_address)
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("123")
+        wd.find_element_by_name("home").send_keys(contact_phonehome)
+
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact_mobile)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(contact_phonework)
+        wd.find_element_by_name("fax").click()
+        wd.find_element_by_name("fax").clear()
+        wd.find_element_by_name("fax").send_keys(contact_fax)
+
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("contact@mail.ru")
-        Select(wd.find_element_by_name("bday")).select_by_visible_text("1")
+        wd.find_element_by_name("email").send_keys(contact_email)
+        wd.find_element_by_name("email2").click()
+        wd.find_element_by_name("email2").clear()
+        wd.find_element_by_name("email2").send_keys(contact_email2)
+        wd.find_element_by_name("email3").click()
+        wd.find_element_by_name("email3").clear()
+        wd.find_element_by_name("email3").send_keys(contact_email3)
+        wd.find_element_by_name("homepage").click()
+        wd.find_element_by_name("homepage").clear()
+        wd.find_element_by_name("homepage").send_keys(contact_homepage)
+
+
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact_bday)
         wd.find_element_by_xpath("//option[@value='1']").click()
         wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("February")
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact_bmonth)
         wd.find_element_by_xpath("//option[@value='February']").click()
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys("2002")
+        wd.find_element_by_name("byear").send_keys(contact_byear)
+
+        wd.find_element_by_name("aday").click()
+        Select(wd.find_element_by_name("aday")).select_by_visible_text(contact_aday)
+        wd.find_element_by_xpath("//div[@id='content']/form/select[3]/option[28]").click()
+        wd.find_element_by_name("amonth").click()
+        Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact_amonth)
+        wd.find_element_by_xpath("//div[@id='content']/form/select[4]/option[12]").click()
+        wd.find_element_by_name("ayear").click()
+        wd.find_element_by_name("ayear").clear()
+        wd.find_element_by_name("ayear").send_keys(contact_ayear)
+
         # submit contact creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[20]").click()
 
