fd11280d9fb91bbc484a217b47205328ebb60792
takulenko <t.akulenko@quality-lab.ru>
2025-02-10 15:33:08 +0300

    Убраны элементы фикстуры, добавлен pytest вместо inittest в test_add_group.   (Урок 2.2.Фикстура)

--- test_add_group.py
+++ test_add_group.py
@@ -1,27 +1,21 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-import unittest
+import pytest
 from application import Application
 from group import Group
 
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
+def test_add_group(app):
+    app.login("admin", "secret")
+    app.create_group(Group("qwe", "asd", "zxc"))
+    app.logout()
 
-    def test_add_group(self):
-        self.app.login("admin", "secret")
-        self.app.create_group(Group("qwe", "asd", "zxc"))
-        self.app.logout()
+def test_add_empty_group(app):
+    app.login("admin", "secret")
+    app.create_group(Group("", "", ""))
+    app.logout()
 
-    def test_add_empty_group(self):
-        self.app.login("admin", "secret")
-        self.app.create_group(Group("", "", ""))
-        self.app.logout()
-
-    def tearDown(self):
-        self.app.destroy()
-
-
-if __name__ == "__main__":
-    unittest.main()
