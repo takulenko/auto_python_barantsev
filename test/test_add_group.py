f772a4b4e4f4ffcad0558a8e9c4bfe82bbf95d0f
takulenko <t.akulenko@quality-lab.ru>
2025-02-11 12:11:56 +0300

    Выделен класс для работы с группами GroupHelper (Урок 2.3.Декомпозиция кода, классы-помощники)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -11,11 +11,11 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group("qwe", "asd", "zxc"))
+    app.group.create(Group("qwe", "asd", "zxc"))
     app.session.logout()
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group("", "", ""))
+    app.group.create(Group("", "", ""))
     app.session.logout()
 
