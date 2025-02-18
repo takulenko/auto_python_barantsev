136370c2ddd04fa2f24373c8068b921ca2d02b41
takulenko <t.akulenko@quality-lab.ru>
2025-02-18 12:18:09 +0300

    Повышаем "интеллектуальность" функций login и logout: проверка текущего состояния сессии (Урок 3.3) LP 8: Реализовать проверку валидности фикстуры и проверку состояния сессии

--- test/conftest.py
+++ test/conftest.py
@@ -10,11 +10,10 @@ def app(request):
     global fixture
     if fixture is None:
         fixture = Application()
-        fixture.session.login(username="admin", password="secret")
     else:
         if not fixture.is_valid():
             fixture = Application()
-            fixture.session.login(username="admin", password="secret")
+    fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
 @pytest.fixture(scope="session", autouse=True)
