a9f07b18c2248a3019f87fa6d8607a30a102b49b
takulenko <t.akulenko@quality-lab.ru>
2025-02-24 20:00:23 +0300

    Сортировка списков перед сравнением (Урок 4.3)

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,12 +1,18 @@
+from tokenize import group
+
 from model.group import Group
 
 def test_modify_group_name (app):
     if app.group.count() == 0:
         app.group.create(Group(name="group for modify name"))
     old_groups = app.group.get_group_list()
-    app.group.edit_first_group(Group(name="Only Name modify"))
+    group = Group(name="Only Name modify")
+    group.id = old_groups[0].id
+    app.group.edit_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_modify_group_header(app):
