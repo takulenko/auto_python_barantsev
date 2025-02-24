9910f674c9970efa25f77c407d7e54e0969e0a08
takulenko <t.akulenko@quality-lab.ru>
2025-02-24 08:53:52 +0300

    Загружаем информацию из тестируемого приложения в виде списков (Урок 4.1)

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,4 +3,7 @@ from model.group import Group
 def test_edit_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="group for edit"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name="qwe Edited", header="asd Edited", footer="zxc Edited"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,16 +3,25 @@ from model.group import Group
 def test_modify_group_name (app):
     if app.group.count() == 0:
         app.group.create(Group(name="group for modify name"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name="Only Name modify"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="group for modify header"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(header="Only header modify"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_group_footer(app):
     if app.group.count() == 0:
         app.group.create(Group(name="group for modify footer"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(footer="Only footer modify"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
