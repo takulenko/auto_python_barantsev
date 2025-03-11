80fa3544db329a8e0b8c88d9ddd52d4b187a76c6
takulenko <t.akulenko@quality-lab.ru>
2025-03-11 11:17:22 +0300

    Передача параметров в тесты из командной строки при запуске (с ошибками) (5.5)

--- fixture/application.py
+++ fixture/application.py
@@ -5,8 +5,16 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognize browser %s" % browser)
+        self.base_url = base_url
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
@@ -21,7 +29,7 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         if not wd.current_url.endswith("/"):
-            wd.get("http://localhost/addressbook/")
+            wd.get(self.base_url)
 
     def return_to_home_page(self):
         wd = self.wd
--- test/conftest.py
+++ test/conftest.py
@@ -1,5 +1,3 @@
-from idlelib.autocomplete import TRY_A
-
 import pytest
 from fixture.application import Application
 
@@ -8,11 +6,13 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseUrl")
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
-            fixture = Application()
+            fixture = Application(browser=browser, base_url=base_url)
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
@@ -22,5 +22,8 @@ def stop(request):
         fixture.session.ensure_logout()
         fixture.destroy()
     request.addfinalizer(fin)
-
     return fixture
+
+    def pytest_addoption(parser):
+        parser.addoption("--browser", action="store", default="firefox")
+        parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
