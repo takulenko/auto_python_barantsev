718fbbec7f9e2bc7ae53b3242420f60092c68f19
takulenko <t.akulenko@quality-lab.ru>
2025-03-14 16:21:22 +0300

    Генерация тестовых данных в формате JSON (6.4)

---
+++ generator/group.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import json
+from string import punctuation
+from model.group import Group
+import random
+import string
+import os.path
+import getopt
+import sys
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of groups", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 5
+f = "data/groups.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10     # + string.punctuation   ' падает
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [Group(name="", header="", footer="")] + [
+    Group(name=random_string("name", 10),
+          header=random_string("header", 20),
+          footer=random_string("footer", 20))
+    for i in range(n)
+]
+
+#testdata = [   перебор полей с null
+#    Group(name=name, header=header, footer=footer)
+#    for name in ["", random_string("name", 10)]
+#    for header in ["", random_string("header", 20)]
+#    for footer in ["", random_string("footer", 20)]
+#]
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
+with open(file, "w") as out:
+    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+
