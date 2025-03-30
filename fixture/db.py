89639305e5f4ff09af0c42385940af2ac2c3f964
takulenko <t.akulenko@quality-lab.ru>
2025-03-30 18:55:41 +0300

    FIX (ДЗ 7.22)

--- fixture/db.py
+++ fixture/db.py
@@ -27,10 +27,10 @@ class DbFixture:
         list = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select id, firstname, lastname, home, mobile, work, email, email2, email3 from addressbook")
+            cursor.execute("select id, firstname, lastname, address, home, mobile, work, email, email2, email3 from addressbook")
             for row in cursor:
-                (id, firstname, lastname, home, mobile, work, email, email2, email3) = row
-                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, home=home, mobile=mobile, work=work,
+                (id, firstname, lastname, address, home, mobile, work, email, email2, email3) = row
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, address=address, home=home, mobile=mobile, work=work,
                                     email=email, email2=email2, email3=email3))
         finally:
             cursor.close()
--- test/test_contact_from_home_page.py
+++ test/test_contact_from_home_page.py
@@ -2,18 +2,40 @@ from model.contact import Contact
 from random import randrange
 import re
 
-# compare contact on home page and db
-def test_contacts_on_home_page_and_db(app, db):
+# compare contact on homepage and db
+def test_contact_on_home_page_and_db(app, db):
     # if contacts list is empty
     if app.contact.count() == 0:
         app.contact.add_contact(Contact(lastname="Ak-ul", firstname="Tata Rastata", address="Addr H: 1",
                                         email="em@m.ru", email2="em2@m.ru", email3="em3@m.ru",
                                         home="home-1231", mobile="(mobile) 11", work="work 12111"))
 
-    contacts_homepage = app.contact.get_contact_list()
-    contacts_db = db.get_contact_list()
+    contacts_list_from_homepage = app.contact.get_contact_list()
+    contacts_list_from_db = db.get_contact_list()
 
-    assert sorted(contacts_homepage, key=Contact.id_or_max) == sorted(contacts_db, key=Contact.id_or_max)
+    for contact_from_homepage in contacts_list_from_homepage:
+        index =  contacts_list_from_db.index(contact_from_homepage)
+        contact_from_db = contacts_list_from_db[index]
+
+        assert contact_from_homepage == contact_from_db
+        assert contact_from_homepage.lastname == contact_from_db.lastname
+        assert contact_from_homepage.firstname == contact_from_db.firstname
+        assert contact_from_homepage.address == contact_from_db.address
+        assert contact_from_homepage.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_db)
+        assert contact_from_homepage.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_db)
+
+def clear(s):
+    return re.sub("[() -]", "", s)
+
+def merge_phones_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear(x),
+                                filter(lambda x: x is not None,
+                                       [contact.home, contact.mobile, contact.work]))))
+
+def merge_emails_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "" and x is not None,
+                                   [contact.email, contact.email2, contact.email3]))
 
 
 '''
@@ -37,16 +59,5 @@ def test_contact_on_home_page_by_index(app):
     assert contact_from_homepage.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
     assert contact_from_homepage.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
-def clear(s):
-    return re.sub("[() -]", "", s)
-
-def merge_phones_like_on_home_page(contact):
-    return "\n".join(filter(lambda x: x != "",
-                            map(lambda x: clear(x),
-                                filter(lambda x: x is not None,
-                                       [contact.home, contact.mobile, contact.work]))))
-
-def merge_emails_like_on_home_page(contact):
-    return "\n".join(filter(lambda x: x != "" and x is not None,
-                                   [contact.email, contact.email2, contact.email3]))
 '''
+
