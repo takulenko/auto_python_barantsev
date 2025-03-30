cf40bd65c1e8d47cb5b9690ce5da00ed7706ebbf
takulenko <t.akulenko@quality-lab.ru>
2025-03-30 19:02:40 +0300

    FIX (ДЗ 7.22)

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -5,7 +5,7 @@ from model.group import Group
 
 def test_add_user_to_group(app, orm, db):
     if len(db.get_contact_list()) == 0:
-        app.user.create(Contact(firstname="fn for add", lastname="ln for add"))
+        app.contact.add_contact(Contact(firstname="fn for add", lastname="ln for add"))
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="group for add"))
     contacts = orm.get_contact_list()
