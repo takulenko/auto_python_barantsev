a2cc332f33031c0e6fa25c6220b75aefc75942d3
takulenko <t.akulenko@quality-lab.ru>
2025-04-03 14:28:32 +0300

    FIX (ДЗ 7.22)

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -3,14 +3,29 @@ from model.contact import Contact
 from model.group import Group
 
 
-def test_add_user_to_group(app, orm, db):
-    if len(db.get_contact_list()) == 0:
-        app.contact.add_contact(Contact(firstname="fn for add", lastname="ln for add"))
+def test_add_contact_to_group(app, orm, db):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="group for add"))
-    contacts = orm.get_contact_list()
-    contact = random.choice(contacts)
     groups = orm.get_group_list()
     group = random.choice(groups)
+
+    print()
+    print(groups)
+    print(group)
+
+    if len(orm.get_contacts_not_in_group(Group(id=group.id))) == 0:
+        app.contact.add_contact(Contact(firstname="fn for add", lastname="ln for add"))
+    contacts = orm.get_contacts_not_in_group(Group(id=group.id))
+    contact = random.choice(contacts)
+
+    print()
+    print(contacts)
+    print(contact)
+
     app.contact.add_contact_to_group(contact.id, group.id)
-    assert contact in orm.get_contacts_in_group(group)
+    new_contacts = orm.get_contacts_in_group(Group(id=group.id))
+
+    print()
+    print(new_contacts)
+
+    assert contact in new_contacts
